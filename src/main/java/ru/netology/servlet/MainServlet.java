59acc88cc5f2839aa8e6bb48da403005b076ce7f
Denis Znobishin <znobishindi@yandex.ru>
2022-11-16 19:45:01 +0300

    HW completed

---
+++ src/main/java/ru/netology/servlet/MainServlet.java
@@ -0,0 +1,67 @@
+package ru.netology.servlet;
+
+import ru.netology.controller.PostController;
+import ru.netology.exception.NotFoundException;
+import ru.netology.repository.PostRepository;
+import ru.netology.service.PostService;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class MainServlet extends HttpServlet {
+
+
+    public static final String API_POSTS = "/api/posts";
+    public static final String API_POSTS_D = "/api/posts/\\d+";
+    public static final String STR = "/";
+    private PostController controller;
+
+    @Override
+    public void init() {
+        final var repository = new PostRepository();
+        final var service = new PostService(repository);
+        controller = new PostController(service);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        final var path = req.getRequestURI();
+        if (path.equals(API_POSTS)) {
+            controller.save(req.getReader(), resp);
+            return;
+        }
+        resp.setStatus(HttpServletResponse.SC_NOT_FOUND);
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        final var path = req.getRequestURI();
+        if (path.equals(API_POSTS)) {
+            controller.all(resp);
+            return;
+        } else if (path.matches(API_POSTS_D)) {
+            final var id = parseId(path);
+            controller.getById(id, resp);
+            return;
+        }
+        resp.setStatus(HttpServletResponse.SC_NOT_FOUND);
+    }
+
+    @Override
+    protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        final var path = req.getRequestURI();
+        if (path.matches(API_POSTS_D)) {
+            final var id = parseId(path);
+            controller.removeById(id, resp);
+            return;
+        }
+        resp.setStatus(HttpServletResponse.SC_NOT_FOUND);
+    }
+
+    private long parseId(String path) {
+        return Long.parseLong(path.substring(path.lastIndexOf(STR) + 1));
+    }
+}
